74aee2d78d7ae2e9ee22b9fac9c53274c8412dd0
bbaral <bbaral@asu.edu>
2018-05-30 23:03:56 -0400

    added some complex shit, Don't bother going through

--- fitness-tracker/src/app/training/new-training/new-training.component.ts
+++ fitness-tracker/src/app/training/new-training/new-training.component.ts
@@ -1,27 +1,32 @@
-import { Exercise } from './../exercise.module';
-import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { AngularFirestore } from 'angularfire2/firestore';
+import { Exercise } from "./../exercise.module";
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { TrainingService } from "../training.service";
 
-import { TrainingService } from '../training.service';
-import { Observable } from 'rxjs';
-
+import { Observable, Subscription } from "rxjs";
+import { map } from "rxjs/operators";
 @Component({
-  selector: 'app-new-training',
-  templateUrl: './new-training.component.html',
-  styleUrls: ['./new-training.component.css']
+  selector: "app-new-training",
+  templateUrl: "./new-training.component.html",
+  styleUrls: ["./new-training.component.css"]
 })
-export class NewTrainingComponent implements OnInit {
-  exercises: Observable<any>;
+export class NewTrainingComponent implements OnInit, OnDestroy {
+  exercises: Exercise[];
+  exerciseSubscription: Subscription;
 
-  constructor(private trainingService: TrainingService, private db: AngularFirestore) { }
+  constructor( private trainingService: TrainingService ) {}
 
   ngOnInit() {
-    this.exercises = this.db.collection('availableExercise').valueChanges();
+    this.exerciseSubscription = this.trainingService.exercisesChanged
+    .subscribe(exercises => (this.exercises = exercises));
+    this.trainingService.fetchAvailableExercises();
   }
 
   onStartTraining(form: NgForm) {
     this.trainingService.startExercise(form.value.exercise);
   }
 
+  ngOnDestroy() {
+    this.exerciseSubscription.unsubscribe();
+  }
 }
